3b17cc7e42170f31c077cdccac4d61f8c2f6ee12
miguelangel2801 <miguelangel2801@gmail.com>
2019-10-17 19:00:40 -0300

    Firmware actualizado, Adquisicion de SONAR LIDAR lista

--- firmware/ec3883.G11/CodeWarrior/Prueba-00/Sources/Events.c
+++ firmware/ec3883.G11/CodeWarrior/Prueba-00/Sources/Events.c
@@ -1,11 +1,11 @@
 /* ###################################################################
 **     Filename    : Events.c
-**     Project     : Prueba-00
+**     Project     : prueba_00
 **     Processor   : MC9S08QE128CLK
 **     Component   : Events
 **     Version     : Driver 01.02
 **     Compiler    : CodeWarrior HCS08 C Compiler
-**     Date/Time   : 2019-10-01, 21:47, # CodeGen: 0
+**     Date/Time   : 2019-10-07, 12:32, # CodeGen: 0
 **     Abstract    :
 **         This is user's event module.
 **         Put your event handler code here.
@@ -30,8 +30,93 @@
 
 #include "Cpu.h"
 #include "Events.h"
-
+extern char flag;
+extern int vars[4];
+extern int US;
 /* User includes (#include below this line is not maintained by Processor Expert) */
+
+/*
+** ===================================================================
+**     Event       :  TI1_OnInterrupt (module Events)
+**
+**     Component   :  TI1 [TimerInt]
+**     Description :
+**         When a timer interrupt occurs this event is called (only
+**         when the component is enabled - <Enable> and the events are
+**         enabled - <EnableEvent>). This event is enabled only if a
+**         <interrupt service/event> is enabled.
+**     Parameters  : None
+**     Returns     : Nothing
+** ===================================================================
+*/
+void TI1_OnInterrupt(void)
+{
+  /* Write your code here ... 
+if(flag==4)flag=0;
+else flag++;*/
+flag=0;
+}
+
+/*
+** ===================================================================
+**     Event       :  AD1_OnEnd (module Events)
+**
+**     Component   :  AD1 [ADC]
+**     Description :
+**         This event is called after the measurement (which consists
+**         of <1 or more conversions>) is/are finished.
+**         The event is available only when the <Interrupt
+**         service/event> property is enabled.
+**     Parameters  : None
+**     Returns     : Nothing
+** ===================================================================
+*/
+void AD1_OnEnd(void)
+{
+  /* Write your code here ... */
+}
+
+
+/*
+** ===================================================================
+**     Event       :  Cap1_OnCapture (module Events)
+**
+**     Component   :  Cap1 [Capture]
+**     Description :
+**         This event is called on capturing of Timer/Counter actual
+**         value (only when the component is enabled - <Enable> and the
+**         events are enabled - <EnableEvent>.This event is available
+**         only if a <interrupt service/event> is enabled.
+**     Parameters  : None
+**     Returns     : Nothing
+** ===================================================================
+*/
+void Cap1_OnCapture(void)
+{
+Cap1_Reset();
+Cap1_GetCaptureValue(&US);
+ 
+}
+
+/*
+** ===================================================================
+**     Event       :  Cap1_OnOverflow (module Events)
+**
+**     Component   :  Cap1 [Capture]
+**     Description :
+**         This event is called if counter overflows (only when the
+**         component is enabled - <Enable> and the events are enabled -
+**         <EnableEvent>.This event is available only if a <interrupt
+**         service/event> is enabled.
+**     Parameters  : None
+**     Returns     : Nothing
+** ===================================================================
+*/
+void Cap1_OnOverflow(void)
+{
+  /* Write your code here ... */
+}
+
 
 /* END Events */
 
